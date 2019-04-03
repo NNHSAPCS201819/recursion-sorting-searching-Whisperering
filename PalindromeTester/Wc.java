8b0e2559bfcfab9925a9629348b3eb6b3c231c6e
Whisperering <emielko@stu.naperville203.org>
2019-04-03 13:36:28 -0500

    pair programming word counts

---
+++ PalindromeTester/Wc.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+/**
+ * Write a description of class Wc here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Wc
+{
+    public static void main (String args[]) throws FileNotFoundException
+    {
+        String str = "";
+        String spaces = "";
+        String periods = "";
+        String file = "";
+        if(args.length > 0)
+        {
+            ;file = args[0];
+        }
+        else
+        {
+            Scanner s = new Scanner(System.in);
+            System.out.print("Enter a file name: ");
+            file = s.next();
+        }
+        File newFile = new File(file);
+        Scanner read = new Scanner(newFile);
+        Scanner readw = new Scanner(newFile);
+        Scanner readl = new Scanner(newFile);
+        read.useDelimiter("[^A-Za-z]+");
+        int characterCount = 0;
+        while(read.hasNext())
+        {
+            str += read.next();
+        }
+        read.close();
+        readw.useDelimiter("[^ ]");
+        int wordCount = 0;
+        while(readw.hasNext())
+        {
+            spaces += readw.next();
+        }
+        readw.close();
+        readl.useDelimiter("[^.]");
+        int lineCount = 0;
+        while(readl.hasNext())
+        {
+            periods += readl.next();
+        }
+        readl.close();
+        characterCount = str.length();
+        wordCount = spaces.length() + 1;
+        lineCount = periods.length();
+        System.out.println("There are " + characterCount + " characters.");
+        System.out.println("There are " + wordCount + " words.");
+        System.out.println("There are " + lineCount + " lines.");
+    }
+}
